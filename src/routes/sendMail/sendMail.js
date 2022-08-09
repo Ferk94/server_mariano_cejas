50d60ad29e569bc093379e0938f9eb50442d9b77
Fernando <fernando.kaganovicz@gmail.com>
2022-08-09 20:15:36 -0300

    another html test

--- src/routes/sendMail/sendMail.js
+++ src/routes/sendMail/sendMail.js
@@ -14,7 +14,18 @@ router.post("/", (req, res, next) => {
     from: MAILER_EMAIL_DEV,
     to: to,
     subject: "Contraseña para ingresar a su book",
-    html: html
+    html: `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <p>Este es el body de mi html</p>
+    </body>
+    </html>`
   };
 
   transporter
